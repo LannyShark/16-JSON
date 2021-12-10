4892f3043488677eed8ca63d680d453bdc594bde
Илья Пермиловский <lannyshark@ya.ru>
2021-12-11 00:56:44 +0300

    Добавьте файлы проекта.

---
+++ 16 JSON/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace _16._1_JSON_Create
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            string path = "Products.json";
+            Product[] products = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                products [i] = new Product();
+                Console.Write("Введите код {0} товара: ", i+1);
+                products[i].Code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите наименование {0} товара: ", i+1);
+                products[i].Name = Console.ReadLine();
+                Console.Write("Введите стоимость {0} товара: ", i+1);
+                products[i].Cost = Convert.ToDouble(Console.ReadLine());
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                };
+                foreach (Product product in products)
+                {
+                    sw.WriteLine(JsonSerializer.Serialize(product, options));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ 16.2 JSON Read/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace _16._2_JSON_Read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            string path = "Products.json";
+            Product[] products = new Product[n];
+            double max = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    products[i] = JsonSerializer.Deserialize<Product>(sr.ReadLine());
+                    if (products[i].Cost > max)
+                    {
+                        max = products[i].Cost;
+                    }
+                }
+            }
+            Console.Write("Самый дорогой товар: ");
+            foreach (Product product in products)
+            {
+                if (product.Cost==max)
+                {
+                    Console.Write("{0} ", product.Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+    }
+}
